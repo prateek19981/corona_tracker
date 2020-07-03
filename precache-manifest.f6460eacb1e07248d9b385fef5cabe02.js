c909fc473b58d415a0273c4dc5738b0eed1a38eb
Prateek gupta <gprateek558@gmail.com>
2020-07-03 19:15:01 +0530

    Updates

---
+++ precache-manifest.f6460eacb1e07248d9b385fef5cabe02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b9912451ac6e5bb3282",
+    "url": "/corona_tracker.git/static/css/main.5a90bbd4.chunk.css"
+  },
+  {
+    "revision": "3b9912451ac6e5bb3282",
+    "url": "/corona_tracker.git/static/js/main.5aac1460.chunk.js"
+  },
+  {
+    "revision": "68a7146670bb46dc2c59",
+    "url": "/corona_tracker.git/static/js/runtime~main.1c426f67.js"
+  },
+  {
+    "revision": "7c08b94c49b174235d90",
+    "url": "/corona_tracker.git/static/js/2.68303633.chunk.js"
+  },
+  {
+    "revision": "4a8b8761351395cbc6d825df0bce8430",
+    "url": "/corona_tracker.git/index.html"
+  }
+];
